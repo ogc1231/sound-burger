2db503fcf3074780b3a37ae64788f780f196f47f
ogc1231 <ogc@live.ie>
2023-06-07 10:49:49 +0000

    Add side and drink models, rename to burger

--- menu/admin.py
+++ menu/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Burger
+from .models import Side
+from .models import Drink
 
-admin.site.register(Menu)
+admin.site.register(Burger)
+admin.site.register(Side)
+admin.site.register(Drink)
--- menu/models.py
+++ menu/models.py
@@ -1,10 +1,29 @@
 from django.db import models
 
 
-class Menu(models.Model):
+class Burger(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField()
     price = models.FloatField()
     addToOrder = models.BooleanField(default=False)
     last_modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+
+class Side(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.FloatField()
+    addToOrder = models.BooleanField(default=False)
+    last_modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Drink(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.FloatField()
+    addToOrder = models.BooleanField(default=False)
+    last_modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
