f4e406c6dcff1949c86e6e0b1da872861f44a277
ogc1231 <ogc@live.ie>
2023-06-07 10:35:41 +0000

    Create menu model

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.FloatField()
+    addToOrder = models.BooleanField(default=False)
+    last_modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
