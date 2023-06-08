e14af0e7eb5a9faaff6fc2c0a9a46134b3ce87d0
ogc1231 <ogc@live.ie>
2023-06-08 13:53:25 +0000

    Link Cloudinary, add Meta class & return self name

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Burger(models.Model):
@@ -8,6 +12,13 @@ class Burger(models.Model):
     addToOrder = models.BooleanField(default=False)
     last_modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    item_image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.name
 
 
 class Side(models.Model):
@@ -17,6 +28,13 @@ class Side(models.Model):
     addToOrder = models.BooleanField(default=False)
     last_modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    item_image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.name
 
 
 class Drink(models.Model):
@@ -26,4 +44,10 @@ class Drink(models.Model):
     addToOrder = models.BooleanField(default=False)
     last_modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    item_image = CloudinaryField('image', default='placeholder')
 
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.name
