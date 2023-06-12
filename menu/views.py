8c684f29b60d7e2c1c3259c4ad0076aa9740b04e
ogc1231 <ogc@live.ie>
2023-06-12 09:08:01 +0000

    Add views for menu

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Burger
+from .models import Side
+from .models import Drink
 
-# Create your views here.
+
+class BurgerList(generic.ListView):
+    model = Burger()
+    queryset = Burger.objects.filter(status=1).order_by('created')
+    template_name = 'menu.html'
+
+
+class SideList(generic.ListView):
+    model = Side()
+    queryset = Side.objects.filter(status=1).order_by('created')
+    template_name = 'menu.html'
+    
+
+class DrinkList(generic.ListView):
+    model = Drink()
+    queryset = Drink.objects.filter(status=1).order_by('created')
+    template_name = 'menu.html'
