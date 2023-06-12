d0a68c2fab75203bbd46cc0aa73ee1603d71d70c
ogc1231 <ogc@live.ie>
2023-06-12 09:55:09 +0000

    Undo changes

--- menu/views.py
+++ menu/views.py
@@ -1,23 +1,3 @@
 from django.shortcuts import render
-from django.views import generic
-from .models import Burger
-from .models import Side
-from .models import Drink
 
-
-class BurgerList(generic.ListView):
-    model = Burger()
-    queryset = Burger.objects.filter(status=1).order_by('created')
-    template_name = 'menu.html'
-
-
-class SideList(generic.ListView):
-    model = Side()
-    queryset = Side.objects.filter(status=1).order_by('created')
-    template_name = 'menu.html'
-    
-
-class DrinkList(generic.ListView):
-    model = Drink()
-    queryset = Drink.objects.filter(status=1).order_by('created')
-    template_name = 'menu.html'
+# Create your views here.
