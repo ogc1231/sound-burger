ce40a4c9c7c7cf845a8f91d8714a88653a23131c
ogc1231 <ogc@live.ie>
2023-06-14 08:41:19 +0000

    Add review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Review(models.Model):
+    name = models.CharField(max_length=100, default='name')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posted")
+    content = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
+
+
