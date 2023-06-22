10e78c1bc293cff491e470b4174c243e3416dfa2
ogc1231 <ogc@live.ie>
2023-06-22 11:31:40 +0000

    Update Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-
-STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Review(models.Model):
-    name = models.CharField(max_length=100, default='name')
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="posted")
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posted")
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    is_public = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['-created']
 
     def __str__(self):
-        return self.name
+        return self.title
