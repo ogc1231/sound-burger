697152c9e5c927d8d095d5255222313d2218c5e4
ogc1231 <ogc@live.ie>
2023-06-22 16:51:58 +0000

    Comment out queryset

--- reviews/views.py
+++ reviews/views.py
@@ -4,7 +4,7 @@ from .models import Review
 
 def get_review_list(request):
     review_list = Review.objects.all
-    queryset = Review.objects.filter(is_public=True).order_by('created')
+    # queryset = Review.objects.filter(is_public=True).order_by('created')
 
     context = {
         'review_list': review_list,
