cc3d1c7c2a320e87f50f13f7fa7fb1be153194fe
ogc1231 <ogc@live.ie>
2023-06-22 11:31:27 +0000

    Add PostAdmin class

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Review
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Review)
+
+@admin.register(Review)
+class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('is_public', 'created')
+    summernote_fields = ('content')
