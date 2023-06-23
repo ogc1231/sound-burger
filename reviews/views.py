23750a126075f92ddd6fffdabe0c04317c50fbea
ogc1231 <ogc@live.ie>
2023-06-23 16:37:37 +0000

    Add edit_review view

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Review
 from .forms import ReviewForm
 
@@ -23,8 +23,22 @@ def add_review(request):
         # content = request.POST.get('content')
         # Review.objects.create(title=title, content=content)
             return redirect('get_review_list')
-    form = ReviewForm
+    form = ReviewForm()
     context = {
         'form': form
     }
     return render(request, 'reviews/add_review.html', context)
+
+
+def edit_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('get_review_list')
+    form = ReviewForm(instance=review)
+    context = {
+        'form': form
+    }
+    return render(request, 'reviews/edit_review.html', context)
