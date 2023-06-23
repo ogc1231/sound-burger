24e9339816b550e496b77536d5e9b4da9c505e59
ogc1231 <ogc@live.ie>
2023-06-23 16:23:16 +0000

    Solve error

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class Review(models.Model):
     title = models.CharField(max_length=100, null=False, unique=True)
     # slug = models.SlugField(max_length=100, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posted", null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posted", null=True)
     content = models.TextField(null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
