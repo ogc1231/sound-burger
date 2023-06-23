30bbc0a0f2ba4387e9c542b6898c27b1ab066388
ogc1231 <ogc@live.ie>
2023-06-23 16:23:27 +0000

    Link ReviewForm

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Review
+from .forms import ReviewForm
 
 
 def get_review_list(request):
@@ -14,10 +15,16 @@ def get_review_list(request):
 
 def add_review(request):
     if request.method == 'POST':
-        author = Review.author
-        title = request.POST.get('title')
-        content = request.POST.get('body')
-        Review.objects.create(title=title, content=content)
-
-        return redirect('get_review_list')
-    return render(request, 'reviews/add_review.html',)
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+        # author = Review.author
+        # title = request.POST.get('title')
+        # content = request.POST.get('content')
+        # Review.objects.create(title=title, content=content)
+            return redirect('get_review_list')
+    form = ReviewForm
+    context = {
+        'form': form
+    }
+    return render(request, 'reviews/add_review.html', context)
