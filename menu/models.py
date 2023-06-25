535989bc353ed281dd8c62f0105f0ec740cfe818
ogc1231 <ogc@live.ie>
2023-06-25 14:51:21 +0000

    Create Customer, Order, OrderItem & Address models

--- menu/models.py
+++ menu/models.py
@@ -20,3 +20,43 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    order_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Food, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    county = models.CharField(max_length=200, null=True)
+    eircode = models.CharField(max_length=200, null=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
