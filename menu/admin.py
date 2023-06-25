c15d46508f4b54107dc7ee211a8ac91ac6d33251
ogc1231 <ogc@live.ie>
2023-06-25 14:52:33 +0000

    Register Models

--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Food
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
 
 
 @admin.register(Food)
