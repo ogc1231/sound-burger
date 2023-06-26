082c8c8f43e84ca021a372b6601809edad9bd4c2
ogc1231 <ogc@live.ie>
2023-06-26 11:01:33 +0000

    Comment out unauthenicated user

--- menu/views.py
+++ menu/views.py
@@ -17,9 +17,9 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-    else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_item': 0}
+    # else:
+    #     items = []
+    #     order = {'get_cart_total': 0, 'get_cart_item': 0}
 
     context = {'items': items, 'order': order}
     return render(request, 'menu/cart.html', context)
@@ -31,9 +31,9 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-    else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_item': 0}
+    # else:
+    #     items = []
+    #     order = {'get_cart_total': 0, 'get_cart_item': 0}
 
     context = {'items': items, 'order': order}
 
