c58b638ecf094b77cbe90dba691ba920237f0cbf
ogc1231 <ogc@live.ie>
2023-06-26 11:01:50 +0000

    Console log user name on button click

--- static/js/script.js
+++ static/js/script.js
@@ -6,5 +6,7 @@ for(let i =0; i < updateButtons.length; i++){
         let productId = this.dataset.product
         let action = this.dataset.action
         console.log("ProductId:", productId, "action:", action)
+
+        console.log("USER:", user)
     })
 }
