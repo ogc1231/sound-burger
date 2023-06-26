399662fdb30d2827a1583beaf7c2089ab604643a
ogc1231 <ogc@live.ie>
2023-06-26 10:37:46 +0000

    Add evenlistener for add to cart btns

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,10 @@
-console.log("hello")
+let updateButtons = document.getElementsByClassName("add-to-cart")
+
+
+for(let i =0; i < updateButtons.length; i++){
+    updateButtons[i].addEventListener("click", function(){
+        let productId = this.dataset.product
+        let action = this.dataset.action
+        console.log("ProductId:", productId, "action:", action)
+    })
+}
