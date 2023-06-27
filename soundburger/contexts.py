5ff17b931cfa571588bdafe67a4106494439f04f
ogc1231 <ogc@live.ie>
2023-06-27 14:35:13 +0000

    Add authenticattion for cart_items

--- soundburger/contexts.py
+++ soundburger/contexts.py
@@ -1,9 +1,16 @@
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from menu.models import Order
 
 
 # get total number of cart items
 def cart_items(request):
-    order = get_object_or_404(Order, customer=request.user, complete=False)
-    cart_items = order.get_cart_items()
-    return {"cart_items": cart_items}
+    if request.user.is_authenticated:
+        order = Order.objects.filter(
+            customer=request.user, complete=False).first()
+        if order is not None:
+            cart_items = order.get_cart_items()
+            return {"cart_items": cart_items}
+        else:
+            return {"cart_items": 0}
+    else:
+        return {"cart_items": 0}
