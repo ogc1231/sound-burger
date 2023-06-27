f26becc56fba1b87927d418b7f56b59423e5c26f
ogc1231 <ogc@live.ie>
2023-06-27 12:11:52 +0000

    Comment out JS

--- static/js/script.js
+++ static/js/script.js
@@ -1,38 +1,38 @@
-let updateButtons = document.getElementsByClassName("add-to-cart")
+// let updateButtons = document.getElementsByClassName("add-to-cart")
 
 
-for(let i = 0; i < updateButtons.length; i++){
-    updateButtons[i].addEventListener('click', function(){
-        let productId = this.dataset.product
-        let action = this.dataset.action
-        console.log('productId:', productId, 'action:', action)
+// for(let i = 0; i < updateButtons.length; i++){
+//     updateButtons[i].addEventListener('click', function(){
+//         let productId = this.dataset.product
+//         let action = this.dataset.action
+//         console.log('productId:', productId, 'action:', action)
 
-        console.log('USER:', user)
-        // updateUserCart()
-    })
-}
+//         console.log('USER:', user)
+//         // updateUserCart()
+//     })
+// }
 
-function updateUserCart(productId, action){
-    console.log('User is logged in')
+// function updateUserCart(productId, action){
+//     console.log('User is logged in')
 
-    let url = '/update_item/'
+//     let url = '/update_item/'
 
-    fetch(url, {
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json',
-            'X-CSRFToken': csrftoken,
-        },
-        body:JSON.stringify({'productId': productId, 'action': action})
-    })
+//     fetch(url, {
+//         method:'POST',
+//         headers:{
+//             'Content-Type':'application/json',
+//             'X-CSRFToken': csrftoken,
+//         },
+//         body:JSON.stringify({'productId': productId, 'action': action})
+//     })
 
-    .then((response) =>{
-        return response.json()
-        // return console.log(response)
-    })
+//     .then((response) =>{
+//         return response.json()
+//         // return console.log(response)
+//     })
 
-    .then((data) =>{
-        console.log('data:', data)
-        // location.reload()
-    })
-}
+//     .then((data) =>{
+//         console.log('data:', data)
+//         // location.reload()
+//     })
+// }
