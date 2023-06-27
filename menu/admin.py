8461b7beb55101835f841125b5fbf29212a06091
ogc1231 <ogc@live.ie>
2023-06-27 10:32:56 +0000

    Add class OrderAdmin & class OrderItemAdmin

--- menu/admin.py
+++ menu/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+# admin.site.register(Customer)
+# admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
 
 
 @admin.register(Food)
 class FoodAdmin(admin.ModelAdmin):
     list_display = ("name", "food_type", "price", "is_public")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("customer", "complete")
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "product", "quantity")
+    ordering = ("-order",)
