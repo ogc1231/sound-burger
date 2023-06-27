76f1e76b177462388069eb7a4c1eb785f991f28e
ogc1231 <ogc@live.ie>
2023-06-27 12:07:08 +0000

    Update url links

--- soundburger/urls.py
+++ soundburger/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from menu.views import get_menu_list, cart, checkout, addItem, updateItem
+from menu.views import get_menu_list, cart, checkout, addItem, updateItem, deleteItem
 from reviews.views import get_review_list, add_review, edit_review, delete_review
 from home.views import home
 
@@ -25,8 +25,9 @@ urlpatterns = [
     path('menu/', get_menu_list, name='get_menu_list'),
     path('cart/', cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
-    path('add_item/<menu_id>', addItem, name='add_item'),
+    path('add_item/<menu_id>/', addItem, name='add_item'),
     path('update_item/', updateItem, name='update_item'),
+    path('delete_item/<menu_id>/', deleteItem, name='delete_item'),
     path('reviews/', get_review_list, name='get_review_list'),
     path('add/', add_review, name='add_review'),
     path('edit/<review_id>', edit_review, name='edit_review'),
