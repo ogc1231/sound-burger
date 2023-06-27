4dea936ac2fdaaf5fac081c1fae9eba393fafc60
ogc1231 <ogc@live.ie>
2023-06-27 10:34:39 +0000

    Comment Customer class

--- menu/models.py
+++ menu/models.py
@@ -22,20 +22,22 @@ class Food(models.Model):
         return self.name
 
 
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+# class Customer(models.Model):
+#     user = models.OneToOneField(
+#         User, on_delete=models.CASCADE, null=True, blank=True)
+#     name = models.CharField(max_length=200, null=True)
+#     email = models.CharField(max_length=200, null=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    customer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True)
     date_ordered = models.DateField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
-    order_id = models.CharField(max_length=200, null=True)
+    # order_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.id)
@@ -53,8 +55,10 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    product = models.ForeignKey(Food, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    product = models.ForeignKey(
+        Food, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
@@ -68,8 +72,10 @@ class OrderItem(models.Model):
 
 
 class ShippingAddress(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    customer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
     county = models.CharField(max_length=200, null=True)
     eircode = models.CharField(max_length=200, null=True)
