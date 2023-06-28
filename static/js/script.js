d5e4160a375f78eb09b71239d918442f40c667f0
ogc1231 <ogc@live.ie>
2023-06-28 20:32:46 +0000

    Delete uneeded code

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-/* jshint esversion: 11 */
-
-const burgerIcon = document.querySelector("#burger-menu");
-const navbarMenu = document.querySelector("#nav-links");
-
-burgerIcon.addEventListener("click", () => {
-    navbarMenu.classList.toggle("is-active");
-    burgerIcon.classList.toggle("is-active");
-});
