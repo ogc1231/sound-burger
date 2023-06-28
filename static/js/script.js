34fffad37815a1cf1121809473fbb1b19fa5b22f
ogc1231 <ogc@live.ie>
2023-06-28 15:14:08 +0000

    Add jshint config setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,47 +1,9 @@
-const burgerIcon = document.querySelector("#burger-menu")
-const navbarMenu = document.querySelector("#nav-links")
+/* jshint esversion: 11 */
+
+const burgerIcon = document.querySelector("#burger-menu");
+const navbarMenu = document.querySelector("#nav-links");
 
 burgerIcon.addEventListener("click", () => {
-    navbarMenu.classList.toggle("is-active")
-    burgerIcon.classList.toggle("is-active")
-    console.log("click")
-})
-
-// let updateButtons = document.getElementsByClassName("add-to-cart")
-
-
-// for(let i = 0; i < updateButtons.length; i++){
-//     updateButtons[i].addEventListener('click', function(){
-//         let productId = this.dataset.product
-//         let action = this.dataset.action
-//         console.log('productId:', productId, 'action:', action)
-
-//         console.log('USER:', user)
-//         // updateUserCart()
-//     })
-// }
-
-// function updateUserCart(productId, action){
-//     console.log('User is logged in')
-
-//     let url = '/update_item/'
-
-//     fetch(url, {
-//         method:'POST',
-//         headers:{
-//             'Content-Type':'application/json',
-//             'X-CSRFToken': csrftoken,
-//         },
-//         body:JSON.stringify({'productId': productId, 'action': action})
-//     })
-
-//     .then((response) =>{
-//         return response.json()
-//         // return console.log(response)
-//     })
-
-//     .then((data) =>{
-//         console.log('data:', data)
-//         // location.reload()
-//     })
-// }
+    navbarMenu.classList.toggle("is-active");
+    burgerIcon.classList.toggle("is-active");
+});
